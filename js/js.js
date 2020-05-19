74829ae506b76e16f3501cb7b952144448678aa7
BOREDDY REDDY VEERA AKHIL KUMAR REDDY <onlineid.br@gmail.com>
2020-05-19 21:30:53 +0530

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,21 @@
+
+function MobileNav() {
+	var MobNav = document.getElementById("Nav_mobile_menu");
+	if (MobNav.style.display === "block") {
+	  MobNav.style.display = "none";
+	} else {
+	  MobNav.style.display = "block";
+	}
+  }
+
+
+function MobileSerach() {
+	var MobS = document.getElementById("Search_mobile_menu");
+	if (MobS.style.display === "block") {
+	  MobS.style.display = "none";
+	} else {
+	  MobS.style.display = "block";
+	}
+  }
+
+
